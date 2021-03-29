c78eddecd3eb7db924aba2ec7aa8f3dba5b02213
@equaresma <equaresma@gmail.com>
2021-03-29 12:58:21 -0300

    Manifest updates

--- source/LuvInBox.Site/ClientApp/src/pages/institutional/luvinbox_manifest.js
+++ source/LuvInBox.Site/ClientApp/src/pages/institutional/luvinbox_manifest.js
@@ -8,17 +8,17 @@ export const LuvInBoxManifest = () => {
     return (
         <div>
             <div className="row">
-                <div className="col-6">
-                    <img src="/images/manifest/mao_costas.jpg" alt="mao nas costas" className="imageSize" />
+                <div className="col-6 imagebox">
+                    <img src="/images/manifest/mao_costas.jpg" alt="mulher sorrindo" className="imageSize" />
                 </div>
                 <div className="col-6 blackBox">
                     <section>
-                        <h1>
+                        <h1 className="font_5">
                             Comprar na<br /><span className="luvInBoxName">LUV IN BOX</span> é ajudar
-                        </h1>
+                            </h1><br />
                         <p>
-                            Quando você compra na <span>LUV IN BOX</span>, garante uma ótima experiência economizando e, ao mesmo tempo, ajuda mulheres, crianças e adolescentes vítimas de abuso sexual e violência doméstica por todo o Brasil.
-                            Afinal, parte do lucro é revertido para o Programa "<span>LUV IN BOX</span><span> Amor, a melhor escolha!"</span> que, através das principais ONG's e OSCIP's do país, presta assistência a milhares de vítimas.
+                            Quando você compra na <span className="luvInBoxName">LUV IN BOX</span>, garante uma ótima experiência economizando e, ao mesmo tempo, ajuda mulheres, crianças e adolescentes vítimas de abuso sexual e violência doméstica por todo o Brasil.
+                            Afinal, parte do lucro é revertido para o Programa "<span className="luvInBoxName">LUV IN BOX</span><span className="spnLuv"> Amor, a melhor escolha!"</span> que, através das principais ONG's e OSCIP's do país, presta assistência a milhares de vítimas.
                         </p>
                     </section>
                 </div>
@@ -26,38 +26,38 @@ export const LuvInBoxManifest = () => {
             <div className="row">
                 <div className="col-6 silverBox">
                     <section>
-                        <h1>
+                        <h1 className="font_5">
                             A mesma compra,<br />
                             um novo significado
-                        </h1>
+                        </h1><br />
                         <p>
-                            Se todos somos convidados a aproveitar a vida neste mundo, temos orgulho em saber que o programa “<span>LUV IN BOX</span> <span>Amor, a melhor escolha!”</span>, juntamente com nossos parceiros, contribuímos para um mundo melhor para todos nós, repleto de amor, saúde e bem-estar.
+                            Se todos somos convidados a aproveitar a vida neste mundo, temos orgulho em saber que o programa <span className="luvInBoxName">“LUV IN BOX</span> <span className="spnLuv">Amor, a melhor escolha!”</span>, juntamente com nossos parceiros, contribuímos para um mundo melhor para todos nós, repleto de amor, saúde e bem-estar.
                         </p>
                     </section>
                 </div>
-                <div className="col-6">
+                <div className="col-6 imagebox">
                     <img src="/images/manifest/mulher_sorrindo.jpg" alt="mulher sorrindo" className="imageSize" />
                 </div>
             </div>
             <div className="row">
                 <div className="col-12">
                     <section>
-                        <div className="row silverBox">
-                            <div className="col-12">
+                        <div className="row silverBoxMin">
+                            <div className="col-12 divCenter" style={{ marginTop: "40px" }}>
                                 <h1>Todos os dias, no Brasil</h1>
                                 <p>Informar é conscientizar</p>
                             </div>
                         </div>
                         <div className="row silverBox">
-                            <div className="col-4">
+                            <div className="col-4 divCenter">
                                 <h1>+800</h1>
                                 <p>mulheres são estupradas</p>
                             </div>
-                            <div className="col-4">
+                            <div className="col-4 divCenter">
                                 <h1>50%</h1>
                                 <p>são crianças, com até 13 anos de idade</p>
                             </div>
-                            <div className="col-4">
+                            <div className="col-4 divCenter">
                                 <h1>+230</h1>
                                 <p>denúncias de violência doméstica são registradas</p>
                             </div>
@@ -66,30 +66,28 @@ export const LuvInBoxManifest = () => {
                 </div>
             </div>
             <div className="row pinkBox">
-                <div className="col-12">
+                <div className="col-12 divCenter">
                     <section>
                         <p>
-                            O programa "<span>LUV IN BOX</span> <span>Amor, a melhor escolha!”</span>" busca mudar essa realidade através da conscientização, mobilização e intervenção da população na causa, assim como do apoio às ONG's, OSCIP's e abrigos que acolhem e protegem as vítimas.
+                            O programa "<span className="luvInBoxNameWhite">LUV IN BOX</span> <span className="spnLuvWhite">Amor, a melhor escolha!”</span>" busca mudar essa realidade através da conscientização, mobilização e intervenção da população na causa, assim como do apoio às ONG's, OSCIP's e abrigos que acolhem e protegem as vítimas.
                         </p>
                     </section>
                 </div>
             </div>
             <div className="row">
                 <div className="col-12 amarelinhaBox">
-                    <section>
-                        <div className="floatingRightBox">
-                            <div className="pinkBox size50"><h1>Área de atuação</h1></div>
-                            <div className="whiteBox size50">
-                                <p>O programa "<span>LUV IN BOX</span> <span>Amor, a melhor escolha!”</span>" atua por todo o Brasil ajudando a transformar em realidade a visão da LUV IN BOX:
+                    <div className="floatingBox">
+                        <div className="pinkBox size50"><h1>Área de atuação</h1></div>
+                        <div className="whiteBox size50">
+                            <p>O programa "<span className="luvInBoxName">LUV IN BOX</span> <span className="spnLuv">Amor, a melhor escolha!”</span>" atua por todo o Brasil ajudando a transformar em realidade a visão da LUV IN BOX:
                             - Tornar o mundo um lugar mais seguro para todas as mulheres, crianças e adolescentes.
                             </p>
-                            </div>
                         </div>
-                    </section>
+                    </div>
                 </div>
             </div>
-            <div className="row pinkBox">
-                <div className="col-12">
+            <div className="row">
+                <div className="col-12 divCenter pinkBox" style={{ height: "160px" }}>
                     <section>
                         <h1>Uma iniciativa por um mundo melhor</h1>
                     </section>
@@ -99,59 +97,86 @@ export const LuvInBoxManifest = () => {
                 <div className="col-12 dancarinaBox">
                     <section>
                         <div>
-                            <h2>
-                                Porque comprar na <span>LUV IN BOX</span><br />é tudo de bom?
-                            </h2>
+                            <h1 className="font_5">
+                                Porque comprar na <span className="luvInBoxName">LUV IN BOX</span><br />é tudo de bom?
+                            </h1>
+                            <br />
                         </div>
-                        <div className="row">
-                            <div className="col-12">
-                                <div className="circle"><p>1. ​Você compra na LUV IN BOX, economiza e se diverte</p></div></div>
+                        <div className="row" style={{ maxWidth: "40%", marginTop: "80px", marginBottom: "80px" }}>
+                            <div className="col-12 divCenter">
+                                <center><div className="circle"><p>1. ​Você compra na LUV IN BOX, economiza e se diverte</p></div></center>
+                                <div className="row">
+                                    <div className="col-6">
+                                        <center><div className="mArrow" style={{ marginLeft: "120px" }}>&#8599;</div></center>
+                                    </div>
+                                    <div className="col-6">
+                                        <center><div className="mArrow" style={{ marginLeft: "-120px" }}>&#8600;</div></center>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                        <div className="row">
-                            <div className="col-6"><div className="circle"><p>3. ​Mais mulheres ganham forças para uma nova vida!</p></div></div>
-                            <div className="col-6"><div className="circle"><p>2. ​A LUV IN BOX contribui no suporte jurídico, psicológico e médico</p></div></div>
+                        <div className="row" style={{ maxWidth: "40%" }}>
+                            <div className="col-6">
+                                <center><div className="circle"><p>3. ​Mais mulheres ganham forças para uma nova vida!</p></div></center>
+                            </div>
+                            <div className="col-6">
+                                <center><div className="circle"><p>2. ​A LUV IN BOX contribui no suporte jurídico, psicológico e médico</p></div></center>
+                            </div>
                         </div>
                     </section>
                 </div>
             </div>
             <div className="row">
                 <div className="col-12">
-                    <section>
-                        <div className="row">
-                            <div className="col-6 lightGrayBox">
-                                <i className="pi pi-shopping-cart"></i>
-                                <h2>Compre</h2>
-                                <p>
-                                    Divirta-se e apimente suas relações. Economize encontrando os melhores preços do mercado, e ainda ajude diretamente na proteção à milhares de mulheres, crianças e adolescentes por todo o Brasil. Aquela mesma compra que você já faz normalmente, agora tem um novo significado: "Amor, a melhor escolha!"
-                                </p>
-                            </div>
-                            <div className="col-6 lightGrayBox">
-                                <i className="pi pi-volume-up"></i>
-                                <h2>Divulgue</h2>
-                                <p>
-                                    O programa "<span>LUV IN BOX</span> <span>Amor, a melhor escolha!”</span>" conta com a parceria de "x" ONGs e abrigos espalhados por "x" estados do país. Nos siga em nossas redes sociais; compartilhe e comente sobre o projeto: Seu apoio é fundamental para a causa.
-                                </p>
-                            </div>
+                    <div className="row">
+                        <div className="col-1 blackBelt"></div>
+                        <div className="col-11 blackBelt">
+                            <center>Ajude-nos a ajudá-las.</center>
                         </div>
-                        <div className="row">
-                            <div className="col-6 lightGrayBox">
-                                <i className="pi pi-star"></i>
-                                <h2>Seja um voluntário</h2>
-                                <p>
-                                    Conheça as ONG's e OSCIP's parceiras do programa, no Brasil, e seja um voluntário.
-                                </p>
-                            </div>
-                            <div className="col-6 lightGrayBox">
-                                <i className="pi pi-money-bill"></i>
-                                <h2>Doe</h2>
-                                <p>
-                                    Apoie ainda mais esse projeto doando online e com segurança, diretamente para as ONG's e OSCIP's parceiras.
-                                </p>
-                            </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-1 blackBelt">
+                            <div className="textRot">Saiba como...</div>
                         </div>
-                    </section>
+                        <div className="col-11">
+                            <section>
+                                <div className="row">
+                                    <div className="col-6 lightGrayBox">
+                                        <i className="pi pi-shopping-cart" style={{ fontSize: "1.5rem", color: "#CC0046" }}></i>
+                                        <h2>Compre</h2><br />
+                                        <p>
+                                            Divirta-se e apimente suas relações. Economize encontrando os melhores preços do mercado, e ainda ajude diretamente na proteção à milhares de mulheres, crianças e adolescentes por todo o Brasil. Aquela mesma compra que você já faz normalmente, agora tem um novo significado: "Amor, a melhor escolha!"
+                                        </p>
+                                    </div>
+                                    <div className="col-6 lightGrayBox">
+                                        <i className="pi pi-volume-up" style={{ fontSize: "1.5rem",  color: "#000" }}></i>
+                                        <h2>Divulgue</h2><br />
+                                        <p>
+                                            O programa "<span className="luvInBoxName">LUV IN BOX</span> <span className="spnLuv">Amor, a melhor escolha!”</span>" conta com a parceria de "x" ONGs e abrigos espalhados por "x" estados do país. Nos siga em nossas redes sociais; compartilhe e comente sobre o projeto: Seu apoio é fundamental para a causa.
+                                        </p>
+                                    </div>
+                                </div>
+                                <div className="row">
+                                    <div className="col-6 lightGrayBox">
+                                        <i className="pi pi-star" style={{ fontSize: "1.5rem", color: "yellow" }}></i>
+                                        <h2>Seja um voluntário</h2><br />
+                                        <p>
+                                            Conheça as ONG's e OSCIP's parceiras do programa, no Brasil, e seja um voluntário.
+                                        </p>
+                                    </div>
+                                    <div className="col-6 lightGrayBox">
+                                        <i className="pi pi-money-bill" style={{ fontSize: "1.5rem", color: "darkGreen" }}></i>
+                                        <h2>Doe</h2><br />
+                                        <p>
+                                            Apoie ainda mais esse projeto doando online e com segurança, diretamente para as ONG's e OSCIP's parceiras.
+                                        </p>
+                                    </div>
+                                </div>
+                            </section>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
+        </div >
     );
 }
--- source/LuvInBox.Site/ClientApp/src/pages/institutional/whoweare.js
+++ source/LuvInBox.Site/ClientApp/src/pages/institutional/whoweare.js
@@ -10,8 +10,21 @@ export const WhoWeAre = () => {
                 <div className="col-12 lightGrayBox">
                     <section>
                         <h1><Trans>who_wew_are_titile</Trans></h1>
+                        <br />
                         <p>
-                            Fundada em XX/XXXX somos ...
+                            Fundada em XX/XXXX, lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin quis ipsum ullamcorper, aliquam libero id, rutrum ligula. Fusce est nulla, suscipit a venenatis vel, cursus non sapien. Aliquam erat volutpat. Ut commodo sodales ante, ut sagittis velit tincidunt nec. Nam vitae ex ex. In blandit, quam sollicitudin rhoncus egestas, ex turpis consectetur leo, nec euismod lacus quam eu lectus. Duis ut felis eget magna scelerisque cursus non nec ipsum. Ut tempor, erat sed suscipit mattis, sem turpis lobortis ante, nec molestie ligula nunc vitae turpis. Nullam egestas nibh arcu, vitae cursus nulla feugiat vitae. Etiam ornare orci at nibh auctor vestibulum. Praesent at elit nunc. Sed iaculis ex vel lectus laoreet, et pulvinar urna cursus. Morbi sit amet malesuada ipsum, ut consectetur nunc. Mauris eleifend sagittis viverra. Donec elementum massa ex, eget hendrerit eros tempor at.
+                        </p>
+                        <p>
+                            Aliquam ornare nibh eget purus finibus, id sagittis lectus fringilla. Pellentesque sit amet interdum diam, in gravida ipsum. Morbi facilisis massa id fringilla semper. In rutrum arcu sit amet vehicula mollis. Curabitur lobortis sem a vestibulum lacinia. Phasellus ornare volutpat nibh vel gravida. Praesent euismod eleifend risus nec porttitor. Nulla a nibh tortor. Morbi gravida ante ut auctor vehicula.
+                        </p>
+                        <p>
+                            Nunc vel feugiat dui. Nunc semper magna ac bibendum scelerisque. Suspendisse tincidunt lobortis sapien at imperdiet. Nulla vitae tincidunt tellus. Aenean ac suscipit elit. Suspendisse in mauris vitae urna porttitor pulvinar. Nam sed sapien ligula. Phasellus blandit odio vel libero pharetra consectetur. Nam sit amet porttitor eros, et auctor risus. Nam pulvinar lorem metus, quis efficitur neque aliquam ac. Integer ac mi sed tellus consectetur feugiat. In hac habitasse platea dictumst.
+                        </p>
+                        <p>
+                            Etiam dapibus, justo nec sodales varius, ipsum ligula cursus leo, vitae finibus mi leo eget risus. Nam ultricies augue nec ullamcorper convallis. Etiam fringilla felis ligula, nec mattis leo ornare eget. Morbi in mattis turpis, quis mattis nunc. Praesent consectetur fringilla sem quis bibendum. Proin tristique purus euismod metus lobortis luctus. Etiam augue est, gravida in mi dictum, consectetur imperdiet dolor. Maecenas lacinia lectus sit amet ex eleifend, ut imperdiet sem bibendum. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed semper nibh id arcu fringilla hendrerit. Integer mollis bibendum dignissim. Donec ut vestibulum ante, ut ullamcorper metus. Nulla sed nulla at ligula iaculis tristique in et diam. Maecenas eleifend, est at ullamcorper cursus, nunc erat fermentum augue, eu rutrum erat lacus in augue. Fusce malesuada justo eu consequat venenatis.
+                        </p>
+                        <p>
+                            Donec aliquam id tellus sed rhoncus. Nam dapibus eget arcu nec viverra. Nunc mollis pellentesque vulputate. Vivamus eu nulla eu turpis sagittis aliquam id sit amet mauris. Donec pretium lorem non quam finibus vulputate. Etiam et est a justo pellentesque luctus in eget lacus. Vivamus vitae sollicitudin nunc, a congue neque. Vivamus vestibulum varius placerat. Cras scelerisque neque quis metus dignissim, non blandit lacus congue. Aenean tincidunt dolor mi, in faucibus diam rhoncus ut. Quisque sed sagittis nisi. Aenean dictum est id leo suscipit, vulputate bibendum ligula mollis. Proin vehicula sit amet mi vel bibendum.
                         </p>
                     </section>
                 </div>
@@ -20,13 +33,13 @@ export const WhoWeAre = () => {
                 <div className="col-6"><img alt="mulher dançando" /></div>
                 <div className="col-6">
                     <section>
-                        <h1>Torne seus desejos realidade</h1>
+                        <h1>Torne seus desejos realidade</h1><br/>
                         <p>
                             Compare os valores. Economize na compra.<br />
                             Divirta-se em sua próxima experiência.
                         </p>
                         <div>
-                            <button>Sou maior de 18 anos</button>
+                            <button className="p-button-help">Sou maior de 18 anos</button>
                         </div>
                     </section>
                 </div>
@@ -34,13 +47,13 @@ export const WhoWeAre = () => {
             <div className="row">
                 <div className="col-6">
                     <section>
-                        <h4>MULHERES EM FOCO</h4>
+                        <h4>MULHERES EM FOCO</h4><br />
                         <p>
                             Compre normalmente,<br />
                             apoie diretamente!
                         </p>
                         <div>
-                            <button>Saiba mais</button>
+                            <button className="p-button-help">Saiba mais</button>
                         </div>
                     </section>
                 </div>
@@ -50,12 +63,12 @@ export const WhoWeAre = () => {
                 <div className="col-6"><img alt="mulher de chapéu" /></div>
                 <div className="col-6">
                     <section>
-                        <h4>Amor, a melhor escolha!</h4>
+                        <h4>Amor, a melhor escolha!</h4><br />
                         <p>
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer et volutpat augue. Nam sagittis fermentum purus vel aliquet.
                         </p>
                         <div>
-                            <button>Saiba mais</button>
+                            <button className="p-button-help">Saiba mais</button>
                         </div>
                     </section>
                 </div>
